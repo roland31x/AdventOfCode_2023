ba0f25107b9a67bc166e52d2779ceb7ad8fe7478
roland31x <roland31x@gmail.com>
2023-12-07 08:03:26 +0200

    asd

---
+++ D07/D07/Program.cs
@@ -0,0 +1,226 @@
+﻿using AOCIn = AOCApi.InputGetter;
+using AOCOut = AOCApi.OutputSubmitter;
+namespace D07
+{
+    internal class Program
+    {
+        // dont forget to change day
+        static int YEAR = 2023;
+        static int DAY = 7;
+        public readonly static string InputPath = @"..\..\..\input.txt";
+        public readonly static string SeshCookie = "cook";
+        public static void Main(string[] args)
+        {
+            List<string> lines = AOCIn.GetInput(InputPath, YEAR, DAY, SeshCookie);
+
+            string part1 = Part1(lines);
+            Console.WriteLine("Part 1 solution: " + part1);
+
+            //string part2 = Part2(lines);
+            //Console.WriteLine("Part 2 solution: " + part2);
+
+            Console.WriteLine("Submit? Y/N");
+            string command = Console.ReadLine();
+            if (command.ToUpper() == "Y")
+            {
+                string resp = AOCOut.Submit(part1, YEAR, DAY, 1, SeshCookie);
+                Console.WriteLine(resp);
+
+                //Thread.Sleep(100);
+
+                //string resp2 = AOCOut.Submit(part2, YEAR, DAY, 2, SeshCookie);
+                //Console.WriteLine(resp2);
+            }
+        }
+        private static string Part1(List<string> lines)
+        {
+            long toreturn = 0;
+
+            List<Hand> hands = new List<Hand>();
+            foreach(string line in lines)
+            {
+                string hand = line.Split(' ')[0];
+                int bid = int.Parse(line.Split(' ')[1]);
+                hands.Add(new Hand(hand, bid));
+            }
+
+            hands.Sort((x1, x2) => -1 * x1.CompareHand(x2));
+
+            for(int i = 0; i < hands.Count; i++)
+            {
+                toreturn += hands[i].Bid * (i + 1);
+            }
+
+            return toreturn.ToString();
+        }
+        public class Hand
+        {
+            static List<string> cards = new List<string>() { "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", };
+            public int[] v = new int[15];
+            public int Bid = 0;
+            public int Rank;
+            public string c;
+            public Hand(string cards, int bid)
+            {
+                Bid = bid;
+                c = cards;
+                for(int i = 0; i < cards.Length; i++)
+                {
+                    v[GetCard(cards[i].ToString())]++;
+                }
+                GetScore();
+            }
+            public override string ToString()
+            {
+                return c;
+            }
+            public bool FiveOfAKind(out int idx)
+            {
+                idx = -1;
+                for(int i = 0; i < v.Length; i++)
+                {
+                    if (v[i] == 5)
+                    {
+                        idx = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public bool FourOfAKind(out int idx)
+            {
+                idx = -1;
+                for (int i = 0; i < v.Length; i++)
+                {
+                    if (v[i] == 4)
+                    {
+                        idx = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public bool FullHouse(out List<int> idxs)
+            {
+                int idx3 = -1;
+                int idx2 = -1;
+                idxs = new List<int>() { idx3, idx2 };
+                return ThreeOfAKind(out idx3) && TwoOfAKind(out idx2, idx3);
+                
+            }
+            public bool ThreeOfAKind(out int idx, int withoutidx = -1)
+            {
+                idx = -1;
+                for (int i = 0; i < v.Length; i++)
+                {
+                    if (i == withoutidx)
+                        continue;
+                    if (v[i] == 3)
+                    {
+                        idx = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public bool TwoOfAKind(out int idx, int withoutidx = -1)
+            {
+                idx = -1;
+                for (int i = 0; i < v.Length; i++)
+                {
+                    if (i == withoutidx)
+                        continue;
+                    if (v[i] == 2)
+                    {
+                        idx = i;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public bool TwoPairs()
+            {
+                return TwoOfAKind(out int idx) && TwoOfAKind(out int _, idx);
+            }
+            public int GetCard(string s)
+            {
+                if (s == "A")
+                    return 14;
+                else if (s == "K")
+                    return 13;
+                else if (s == "Q")
+                    return 12;
+                else if (s == "J")
+                    return 11;
+                else if (s == "T")
+                    return 10;
+                else
+                    return int.Parse(s);
+            }
+            public void GetScore()
+            {
+                if (FiveOfAKind(out int _))
+                    Rank = 10;
+                else if (FourOfAKind(out int _))
+                    Rank = 9;
+                else if (FullHouse(out _))
+                    Rank = 8;
+                else if (ThreeOfAKind(out int _))
+                    Rank = 7;
+                else if (TwoPairs())
+                    Rank = 6;
+                else if (TwoOfAKind(out int _))
+                    Rank = 5;
+                else
+                    Rank = 4;
+            }
+            public int CompareHand(Hand other)
+            {
+                if (this.Rank != other.Rank)
+                    return this.Rank.CompareTo(other.Rank);
+                else
+                {
+                    int maxsc = 0;
+                    switch(Rank)
+                    {
+                        case 10:
+                            maxsc = 5;
+                            break;
+                        case 9:
+                            maxsc = 4;
+                                break;
+                        case 8:
+                        case 7:
+                            maxsc = 3;
+                            break;
+                        case 6:
+                        case 5:
+                            maxsc = 2;
+                            break;
+                        default:
+                            maxsc = 1;
+                            break;
+                    }
+                    while(maxsc > 0)
+                    {
+                        int idx = 14;
+                        while (idx >= 0)
+                            idx--;
+                        if (idx == -1)
+                            maxsc--;
+                        else
+                            return v[idx].CompareTo(other.v[idx]);
+                    }                                      
+                }
+            }
+        }
+        private static string Part2(List<string> lines)
+        {
+            long toreturn = 0;
+
+
+
+            return toreturn.ToString();
+        }
+    }
+}
